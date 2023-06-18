8a4d6c32111e2e807f7c667a76324ffe93628906
Adelshin <ai.adelshin@stalmail.org>
2023-06-18 15:09:02 +0500

    iter1-iter4

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"context"
 	"log"
-	"net/http"
-	"time"
 
 	"github.com/Orendev/go-loyalty/internal/app"
-	"github.com/Orendev/go-loyalty/internal/client"
 	"github.com/Orendev/go-loyalty/internal/config"
 	"github.com/Orendev/go-loyalty/internal/logger"
-	"github.com/Orendev/go-loyalty/internal/models"
-	"github.com/Orendev/go-loyalty/internal/repository"
-	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -25,39 +18,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	shutdownTimeout := 10 * time.Second
-	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
-	defer cancel()
-
-	repo, err := repository.NewRepository(cfg.Database.URI)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func() {
-		err = repo.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	err = repo.Bootstrap(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	accrualChain := make(chan models.Accrual, cfg.Size)
-
-	a := app.NewApp(ctx, repo, accrualChain)
-
-	_, err = client.NewHTTPClient(context.Background(), repo, cfg.AccrualSystem.Addr, accrualChain)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	srv := &http.Server{
-		Addr:    cfg.Server.Addr,
-		Handler: a.Routes(chi.NewRouter()),
-	}
-
-	log.Fatal(srv.ListenAndServe())
+	app.Run(cfg)
 }
